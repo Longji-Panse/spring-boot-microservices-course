07c09e17921f888230e3cc408647de9d6e81210d
Longji Panse <longjipanse@gmail.com>
2026-04-28 15:55:03 +0100

    Catalog service setup: infra with github actions

--- catalog-service/src/test/java/com/practice/catalogservice/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/practice/catalogservice/TestCatalogServiceApplication.java
@@ -2,28 +2,13 @@ package com.practice.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestCatalogServiceApplication {
 
-	@Bean
-	@Primary
-	@ServiceConnection
-	PostgreSQLContainer<?> postgreSQLContainer() {
-		return new PostgreSQLContainer<>(
-				DockerImageName.parse("postgres:17"));
-	}
-
-	public static void main(String[] args) {
-		SpringApplication
-				.from(CatalogServiceApplication::main)
-				.with(TestcontainersConfiguration.class)
-				.run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
